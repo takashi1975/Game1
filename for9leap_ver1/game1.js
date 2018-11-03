07f8e876371221a262aecaf43c7b9c200a0fc356
Shimeji Ozaki <sdkfz181tiger@gmail.com>
2018-11-03 11:59:44 +0900

    バグ対応

--- for9leap_ver1/game1.js
+++ for9leap_ver1/game1.js
@@ -6,21 +6,3 @@ function game1(){
   scene.backgroundColor = "red";
   scene.on(Event.TOUCH_START, function(){nextScene();});
 }
-
-// ゲーム2
-function game2(){
-  console.log("Game2");
-  var scene = new Scene();
-  core.replaceScene(scene);
-  scene.backgroundColor = "blue";
-  scene.on(Event.TOUCH_START, function(){nextScene();});
-}
-
-// ゲーム3
-function game3(){
-  console.log("Game3");
-  var scene = new Scene();
-  core.replaceScene(scene);
-  scene.backgroundColor = "yellow";
-  scene.on(Event.TOUCH_START, function(){nextScene();});
-}
